f4337455b8eebebe71f0040377857c72a429531e
wlcreate <waverley.nyc@gmail.com>
2020-09-12 18:50:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("text")
+    .innerHTML = "This is really cool!"
+  });
+  
+// Use the event's callback function to target the paragraph element with id="text" 
+// and replace the text with "This is really cool!"
