e071e6a258d5873887db7fe3787e79002ae7d6e9
fbado66 <fbado66@gmail.com>
2020-09-14 17:33:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+} );
+
+function updateDOM() {
+    let update = document.querySelector("#text")
+    update.innerHTML = 'This is really cool!';
+
+}
