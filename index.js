00dda21cdc15e8360d6cdabab4c3e12f935833bf
Elronia <zar.catherine@gmail.com>
2020-09-14 15:37:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+function domContentLoaded() {
+    document.addEventListener("DOMContentLoaded", function(event) {
+      updateDOM()
+    });
+}
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+} );
+
+function updateDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+}
