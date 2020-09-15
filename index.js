96ea83395a67990e029e7d6ee668dda4c1d259ef
attenbl19 <attenboroughl@protonmail.com>
2020-09-14 22:44:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    newDOM()
+  });
+  
+  function newDOM() {
+      document.getElementById("text")
+      .innerHTML = "This is really cool!";
+  }
